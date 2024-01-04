e033c49d9c00151b705871e486a6ce1ccddd33eb
vivi-toto <fernandavitoria856@gmail.com>
2024-01-04 19:57:30 -0400

    exerciocio08 adicionado

---
+++ exercicio08.js
@@ -0,0 +1,15 @@
+/* Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+ tem 1.8 de altura e seu imc é de 25.925925925925924 */
+
+ const nome = 'Luiz Otávio';
+ const sobrenome = 'Miranda';
+ const idade = 30;
+ const peso = 84;
+ const altura = 1.80;
+ let anoAtual = 2010; 
+ let imc =  peso / (altura *altura);
+ let anoNascimento = anoAtual - idade;
+
+ console.log(nome, sobrenome);
+
+
