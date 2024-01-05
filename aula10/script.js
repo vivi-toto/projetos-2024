23e341deaef778eed688ea4feaeb5128d4729e94
vivi-toto <fernandavitoria856@gmail.com>
2024-01-04 22:12:02 -0400

    exercicio 10 resolvido

---
+++ aula10/script.js
@@ -0,0 +1,5 @@
+const num1 = prompt("Digite o primeiro número:");
+const num2 = prompt("Digite o segundo número número:");
+
+let soma = parseInt(num1) + parseInt(num2);
+alert(`a soma dos numeros é ${soma} `);
