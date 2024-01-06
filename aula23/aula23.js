4ddfff28602e481c342bff8920c517e8328f0d0e
vivi-toto <fernandavitoria856@gmail.com>
2024-01-06 12:28:26 -0400

    exercicio23

--- aula23/aula23.js
+++ aula23/aula23.js
@@ -6,3 +6,6 @@ varA = 'B';
 varB = 'C';
 varC = 'A';
 
+console.log(`o valor atual de varA é ${varA}`)
+console.log(`o valor atual de varB é ${varB}`)
+console.log(`o valor atual de varC é ${varC}`)
