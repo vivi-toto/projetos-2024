cb167157ebca0c2ac8d02811b80948d2d1a45fc9
vivi-toto <fernandavitoria856@gmail.com>
2024-01-06 21:34:58 -0400

    "feat: exercicio resolovido"

--- aula23/aula23.js
+++ aula23/aula23.js
@@ -2,9 +2,15 @@ let varA = 'A';
 let varB = 'B';
 let varC = 'C';
 
-varA = 'B';
-varB = 'C';
-varC = 'A';
+// varA = 'B';
+// varB = 'C';
+// varC = 'A';
+
+const varTempA = varA
+varA = varB; // varA = 'B'
+varB = varC; // varB = 'C'
+varC = varTempA; // varC = 'A'
+
 
 console.log(`o valor atual de varA é ${varA}`)
 console.log(`o valor atual de varB é ${varB}`)
