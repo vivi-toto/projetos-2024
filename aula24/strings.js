ec6864c2b734f733993f1c0992f2044cbf280084
vivi-toto <fernandavitoria856@gmail.com>
2024-02-08 09:59:41 -0400

    teste

--- aula24/strings.js
+++ aula24/strings.js
@@ -1,5 +1,5 @@
 let myString = "Uma string qualquer";
 
-console.log(myString.match(/[a-z]/g));
+console.log(myString.match(/[a-zA-Z]/g));
 
-console.log(myString.slice(0, 10))
+// console.log(myString.slice(0, 10))
