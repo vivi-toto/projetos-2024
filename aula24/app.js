e485c6895b419cbfa871e62407a921b84c2eb54d
vivi-toto <fernandavitoria856@gmail.com>
2024-02-08 12:22:19 -0400

    teste

---
+++ aula24/app.js
@@ -0,0 +1,12 @@
+function StringChanges(str) { 
+    str = "abNcdN"; 
+       for (let i=0; i <= str.length; i++) {
+        let letterOneRegex = /N/;
+        console.log(letterOneRegex)
+       }
+        
+        return str; 
+               
+      }
+      
+      module.exports = StringChanges;
