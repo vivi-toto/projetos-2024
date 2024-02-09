7daf017b91d727aace1662247be54d7d5ddf407c
vivi-toto <fernandavitoria856@gmail.com>
2024-02-08 22:49:02 -0400

    0802

--- aula24/app.js
+++ aula24/app.js
@@ -1,9 +1,9 @@
 function StringChanges(str) { 
     str = "abNcdN"; 
-       for (let i=0; i <= str.length; i++) {
-        let letterOneRegex = /N/;
-        console.log(letterOneRegex)
-       }
+      //  for (let i=0; i <= str.length; i++) {
+      //   let letterOneRegex = /N/;
+      //   console.log(letterOneRegex)
+      //  }
         
         return str; 
                
--- aula24/strings.js
+++ aula24/strings.js
@@ -4,18 +4,37 @@
 
 // console.log(myString.slice(0, 10))
 
-// let umaString = "o rato roeu a roupa do rei de roma";
-// console.log(umaString.replace(/r/g, "#"));
+let umaString = "abNcdN";
+let letraN = "N";
+let resultado = " ";
 
-function StringChanges(str) { 
-    str = "abNcdN"; 
-       for (let i=0; i <= str.length; i++) {
-        let letterOneRegex = /N/;
-        console.log(letterOneRegex)
-       }
+for (i = 0; i < umaString.length; i++) {
+    let charAtual = string[i];
+    // let posicao = umaString.indexOf("N");
+    if (charAtual = "N") {
+        let charSubstituido = String.fromCharCode(charAtual.charCodeAt(0) - 1);
+            resultado += charSubstituido;
+        } else {
+            resultado += charAtual;
+        }
+        // console.log(umaString.replace(/N/g," "))
+    }
+
+
+console.log(umaString);
+
+
+
+
+
+// function StringChanges(str) { 
+//     str = "abNcdN"; 
+//        console.log(StringChanges.replace)
         
-        return str; 
+//         return str; 
                
-      }
-      
-      export default StringChanges;
+//       }
+
+
+
+
