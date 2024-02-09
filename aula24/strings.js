5f4465d9ca7b907f9fa0c669758e52347ddd37e2
vivi-toto <fernandavitoria856@gmail.com>
2024-02-08 20:57:10 -0400

    teste

--- aula24/strings.js
+++ aula24/strings.js
@@ -4,5 +4,18 @@
 
 // console.log(myString.slice(0, 10))
 
-let umaString = "o rato roeu a roupa do rei de roma";
-console.log(umaString.replace(/r/g, "#"));
+// let umaString = "o rato roeu a roupa do rei de roma";
+// console.log(umaString.replace(/r/g, "#"));
+
+function StringChanges(str) { 
+    str = "abNcdN"; 
+       for (let i=0; i <= str.length; i++) {
+        let letterOneRegex = /N/;
+        console.log(letterOneRegex)
+       }
+        
+        return str; 
+               
+      }
+      
+      export default StringChanges;
